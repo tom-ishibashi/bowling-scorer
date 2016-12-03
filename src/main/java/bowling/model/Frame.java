7d8a25c667b4aaa6570f1b1f9728f19f9f1176ec
tomoharu ishibashi <bassyMac@r58-98-41-8.usr.ntt-bp.net>
2016-12-03 18:04:16 +0900

    create model Frame and Pin

---
+++ src/main/java/bowling/model/Frame.java
@@ -0,0 +1,36 @@
+package bowling.model;
+
+import java.util.List;
+
+/**
+ * Created by bassyMac on 2016/12/03.
+ */
+public class Frame {
+    private int frame;
+    private List<Pin> pins;
+    private int score;
+
+    public int getFrame() {
+        return frame;
+    }
+
+    public void setFrame(int frame) {
+        this.frame = frame;
+    }
+
+    public List<Pin> getPins() {
+        return pins;
+    }
+
+    public void setPins(List<Pin> pins) {
+        this.pins = pins;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ src/main/java/bowling/model/Pin.java
@@ -0,0 +1,16 @@
+package bowling.model;
+
+/**
+ * Created by bassyMac on 2016/12/03.
+ */
+public class Pin {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
