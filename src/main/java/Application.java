25feb496b79981a1b2c719861aee9f6ee3f909b6
tomoharu ishibashi <bassyMac@r58-98-41-8.usr.ntt-bp.net>
2016-12-03 17:57:57 +0900

    create Application and Scorer class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,10 @@
-package PACKAGE_NAME;
+import bowling.controller.Scorer;
 
 /**
  * Created by bassyMac on 2016/12/03.
  */
 public class Application {
+    public static void main(String[] args) {
+        new Scorer().execute();
+    }
 }
---
+++ src/main/java/bowling/controller/Scorer.java
@@ -0,0 +1,13 @@
+package bowling.controller;
+
+/**
+ * Created by bassyMac on 2016/12/03.
+ */
+public class Scorer {
+    public Scorer() {
+    }
+
+    public void execute() {
+
+    }
+}
